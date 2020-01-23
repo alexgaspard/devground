6a54053b62ce4855bb5bc5e08e354091d3bff2d3
Alexandre Gaspard <alexgaspard@gmail.com>
2020-01-23 09:44:30 +0000

    Allow to choose mariadb or h2 database
    Signed-off-by: Alexandre Gaspard <alexgaspard@gmail.com>

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 A spring-boot application used to test different development techniques from here and there.
 The original structure follows hexagonal architecture and comes from [this project](https://github.com/gshaw-pivotal/spring-hexagonal-example).
 
+The code style is inconsistent as I wanted to try out different ways of achieving something.
+For example, to benefit from the hexagonal architecture, the wiring can be in `ApplicationConfiguration.java` for pure Java classes or in `application/pom.xml` for Spring instanced classes.
+Choosing a h2 database or a mariadb instance needs only to change the dependency used for `offer-persistence-?-adapter` in `application/pom.xml`.
+
 ## Other inspirations
 
 - [API versioning](https://medium.com/@XenoSnowFox/youre-thinking-about-api-versioning-in-the-wrong-way-6c656c1c516b)
--- offer-persistence-h2-adapter/src/main/java/me/alex/devground/config/DemoConfiguration.java
+++ offer-persistence-h2-adapter/src/main/java/me/alex/devground/config/DemoConfiguration.java
@@ -15,8 +15,8 @@ public class DemoConfiguration {
     @Bean
     public CommandLineRunner demoData(OfferPersister persister) {
         return args -> {
-            persister.persist(new Offer("", "First generated offer", Instant.now(), Duration.ofMinutes(2), Currency.getInstance(Locale.UK), 12.34, false));
-            persister.persist(new Offer("", "Second generated offer", Instant.now().minusSeconds(50), Duration.ofMinutes(1), Currency.getInstance(Locale.UK), 56.78, true));
+            persister.persist(new Offer("", "First generated offer (h2)", Instant.now(), Duration.ofMinutes(2), Currency.getInstance(Locale.UK), 12.34, false));
+            persister.persist(new Offer("", "Second generated offer (h2)", Instant.now().minusSeconds(50), Duration.ofMinutes(1), Currency.getInstance(Locale.UK), 56.78, true));
         };
     }
 }
