808ef0dfef4a8dcde54000fcd8d27413878103de
Alexandre Gaspard <alexgaspard@gmail.com>
2020-01-23 09:31:17 +0000

    Make it work with mariadb
    Signed-off-by: Alexandre Gaspard <alexgaspard@gmail.com>

--- offer-persistence-mariadb-adapter/src/main/java/me/alex/devground/config/DemoConfiguration.java
+++ offer-persistence-mariadb-adapter/src/main/java/me/alex/devground/config/DemoConfiguration.java
@@ -1,22 +1,16 @@
 package me.alex.devground.config;
 
 
-import me.alex.devground.models.*;
-import me.alex.devground.ports.*;
-import org.springframework.boot.*;
 import org.springframework.context.annotation.*;
-
-import java.time.*;
-import java.util.*;
 
 @Configuration
 public class DemoConfiguration {
 
-    @Bean
-    public CommandLineRunner demoData(OfferPersister persister) {
-        return args -> {
-            persister.persist(new Offer("", "First generated offer", Instant.now(), Duration.ofMinutes(2), Currency.getInstance(Locale.UK), 12.34, false));
-            persister.persist(new Offer("", "Second generated offer", Instant.now().minusSeconds(50), Duration.ofMinutes(1), Currency.getInstance(Locale.UK), 56.78, true));
-        };
-    }
+//    @Bean
+//    public CommandLineRunner demoData(OfferPersister persister) {
+//        return args -> {
+//            persister.persist(new Offer("", "First generated offer", Instant.now(), Duration.ofMinutes(2), Currency.getInstance(Locale.UK), 12.34, false));
+//            persister.persist(new Offer("", "Second generated offer", Instant.now().minusSeconds(50), Duration.ofMinutes(1), Currency.getInstance(Locale.UK), 56.78, true));
+//        };
+//    }
 }
